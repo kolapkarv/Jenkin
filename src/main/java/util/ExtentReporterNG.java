4136398d05551883eae5cc73b6bb748c26fdac6c
kolapkarv <117445863+kolapkarv@users.noreply.github.com>
2022-11-08 17:02:39 +0530

    Update ExtentReporterNG.java

--- src/main/java/util/ExtentReporterNG.java
+++ src/main/java/util/ExtentReporterNG.java
@@ -24,7 +24,7 @@ public class ExtentReporterNG implements IReporter {
 
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
 			String outputDirectory) {
-		extent = new ExtentReports(outputDirectory + File.separator
+		extent = new ExtentReports("/home/tech-trail/MY-workspace/xuriti/target" + File.separator
 				+ "Extent.html", true);
 
 		for (ISuite suite : suites) {
